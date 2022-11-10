0bbda4cd174d9a844d5d32658c4e83ddf3b4b723
orya <orya.sp@gmail.com>
2022-11-10 17:41:33 +0200

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import User from './User';
+import '../styles/home.css'
+
+class Home extends Component {
+
+    render() {
+        const users = this.props.users 
+        return (    
+            <div>
+               <h1>WHO'S WATCHING?</h1>
+                <div id='users-container'>
+                    {users.map(u => <User user={u}  key={u.name}/>)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
