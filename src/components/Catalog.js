0fc6f99de9c4184973a47ecb8f92205b3019503f
orya <orya.sp@gmail.com>
2022-11-11 16:15:54 +0200

    Add Container to Catalog component

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,45 +1,73 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import CatalogHeader from './CatalogHeader'
+import Rented from './Rented';
+import Movie from './Movie';
 import '../styles/catalog.css'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 
 class Catalog extends Component {
     constructor() {
         super();
         this.state = {
             budget: 0,
-            searchInput: ""
+            searchInput: "",
+            rented: 0,
+            columnsPerRow: 5
         }
     }
 
     getUser = () => {
         const queryString = require('query-string');
-        console.log(this.props);
         const queryParams = queryString.parse(this.props.location.search);
         const userName =  queryParams.user
-        return this.props.state.users.filter(u => u.name === userName)
+        return this.props.state.users.find(u => u.name === userName)
     }
 
     updateSearch = input => {
         this.setState({searchInput: input})
-        console.log(input);
+        console.log(input);     ///////////
     }
 
     updateBudget = budget => {
         this.setState({budget: budget})
     }
 
+    getRented = () => {
+        if(this.state.rented > 0) {
+            const movies = this.props.state.movies
+            const rented = movies.filter(m => m.isRented)
+            return <Rented movies={rented}/>
+        }
+    }
+
+    getColumnsForRow = () => {
+        const movies = this.props.state.movies
+        return movies.map(m => <Movie movie={m} key={m.title + m.year}/>)
+    }
+
 
 
     render() {
         const user = this.getUser()
-
+        let budget = user.budget
 
         return (
             <div>
-               <CatalogHeader user={user} updateSearch={this.updateSearch} updateBudget={this.updateBudget}/>
+               <CatalogHeader user={user} updateSearch={this.updateSearch} budget={budget}/>
+               <div>{this.getRented()}</div>
+                
+
                <h2>Catalog</h2>
 
+               <Container>
+                    <Row xs={1} md={this.state.columnsPerRow}>
+                        {this.getColumnsForRow()}
+                    </Row>
+                </Container>  
             </div>
         );
     }
