5be6b0d8763abff46bb31301144069d60df6aab1
orya <orya.sp@gmail.com>
2022-11-13 15:47:21 +0200

    Add  carousel to movies in Rented component

--- src/components/Rented.js
+++ src/components/Rented.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import Movie from './Movie';
-// import '../styles/rented.css'
+
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+
 
 class Rented extends Component {
+    constructor() {
+        super()
+        this.state = { 
+            responsive: {
+                0: {
+                    items: 1
+                },
+                512: {
+                    items: 3
+                },
+                1024: {
+                    items: 6
+                },
+                2000: {
+                    items: 8
+                }
+            }
+        }
+    }
+
+    handleDragStart = (e) => e.preventDefault();
 
     getRented = () => {
         const movies = this.props.movies
-        return movies.map(m => <Movie movie={m} key={m.title + m.year} rent={this.props.rent}/>)
+        return movies.map(m => <Movie movie={m} key={m.id} rent={this.props.rent} onDragStart={this.handleDragStart} className='rented-movies'/>)
     }
     
     render() { 
+
         return ( 
             <div>
                 <h4>Rented</h4>
-                {this.getRented()}
+                <AliceCarousel mouseTracking items={this.getRented()} responsive={this.state.responsive} className='rented-carousel'/>
             </div> 
         );
     }
